d6a96e8d1f225bd6d2c4f4d1bd1e547553ae7977
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-02-17 05:55:59 +0300

    123123

--- src/Section5/src/main.java
+++ src/Section5/src/main.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class main {
+    public static void main(String[] args) {
+        double kilometers = (100 * 1.60344);
+        int highScore = 50;
+
+        if (highScore > 25) {
+            highScore = 1000 + highScore;
+        }
+
+        int health = 100;
+        if((health < 25) && (highScore > 1000)) {
+            highScore = highScore - 1000;
+        }
+    }
 }
