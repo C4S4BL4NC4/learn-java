65fd80e3dcfa078aa044aa44d1422ab6469a34ac
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-02-17 05:16:44 +0300

    new

---
+++ src/helloworld/src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        boolean isAlien = true;
+        if (isAlien == false)
+            System.out.println("Not an alien");
+        else
+            System.out.println("Is an alien");
+    }
+}
