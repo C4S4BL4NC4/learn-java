0a8daddd5a7180e6874712cfa7baa16639a77aef
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-02-19 22:10:10 +0300

    Add input handling methods to GenInput class and enhance Loopz class with loop examples

--- src/Section6/src/GenInput.java
+++ src/Section6/src/GenInput.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class GenInput {
+    public static void main(String[] arg) {
+        getInputFromConsole();
+    }
+    // No default parameters in JAVA
+    public static String getInputFromConsole() {
+        String name = System.console().readLine("Hi, What's your Name? ");
+        System.out.println("Hi " + name + ", Thanks for taking the course!");
+        return "";
+    }
+
+    public static String getInputFromScanner() {
+        Scanner scanner = new Scanner(System.in);
+        return "";
+    }
+
+//    try {
+//
+//    } catch (Exception e ) {
+//
+//    }
+
 }
--- src/Section6/src/Loopz.java
+++ src/Section6/src/Loopz.java
@@ -3,5 +3,22 @@ public class Loopz {
         for(int x = 0; x < 10; x++) {
            System.out.println(x);
         }
+
+        while(true){
+            System.out.println("While loop");
+            break;
+        }
+
+        do {
+            System.out.println("Do While loop");
+            int age = 23;
+            String name = "Rocky";
+            System.out.printf("Age: %d\nName: %s\n", age, name);
+            break;
+        } while (true);
+
+        int dateOfBirth = Integer.parseInt("1999");
+        String name = "Alice".toUpperCase();
+        System.out.println(name);
     }
 }
