c09159bb693921b59a18107559f4e001447a8e87
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-02-22 07:01:24 +0300

    Refactor MinMaxChallenge to improve input handling and add min/max logic; enhance user prompts and program flow

--- src/Section6/src/MinMaxChallenge.java
+++ src/Section6/src/MinMaxChallenge.java
@@ -4,14 +4,35 @@ public class MinMaxChallenge {
 
     public static void main(String[] args) {
         int min,
-            max,
-            current = 0;
-        System.out.println("Enter a value: ");
+            max = 0;
+        System.out.println("Enter a value(integer): ");
         Scanner scanner = new Scanner(System.in);
-        if (firstEntry == 0 || !scanner.hasNextInt()) {
+
+        int current = scanner.nextInt();
+
+        if (current == 0) {
             System.out.println("Program terminated");
+            scanner.close();
         } else {
             System.out.println("Continue");
+            min = current;
+            max = current;
+            while (true) {
+                System.out.printf("Enter a value: ");
+                current = scanner.nextInt();
+                if (current >= min && current > max) {
+                    max = current;
+                    System.out.printf("Min: %d || Max: %d\n", min, max);
+                    continue;
+                } else if (current <= min && current < min) {
+                    min = current;
+                    System.out.printf("Min: %d || Max: %d\n", min, max);
+                    continue;
+                } else if (current == 0) {
+                    break;
+                }
+                System.out.printf("Min: %d || Max: %d\n", min, max);
+            }
         }
     }
 }
