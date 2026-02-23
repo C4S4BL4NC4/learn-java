a50864560b8d7d05b4d4b7bb4283c012aae31f96
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-02-23 14:29:22 +0300

    Implement Calculator, Carpet, and Floor classes with constructors and methods; add Main class for demonstration

--- src/Section7/src/costChallenge/src/Floor.java
+++ src/Section7/src/costChallenge/src/Floor.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class Floor {
+
+    private double width, length;
+
+    public Floor(double width, double length) {
+        if (length < 0 || width < 0) {
+            if (length < 0 && width >= 0) {
+                this.length = 0;
+                this.width = width;
+            } else if (width < 0 && length >= 0) {
+                this.width = 0;
+                this.length = length;
+            } else {
+                this.length = 0;
+                this.width = 0;
+            }
+        } else {
+            this.length = length;
+            this.width = width;
+        }
+    }
+
+    public double getArea() {
+        return this.width * this.length;
+    }
 }
