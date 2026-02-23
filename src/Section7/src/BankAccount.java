f64b00687eaf95c979cb1d379f69677d37a35961
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-02-23 09:15:49 +0300

    Refactor BankAccount and ConstructorChallenge classes; update Main class for improved readability and constructor usage demonstration

--- src/Section7/src/BankAccount.java
+++ src/Section7/src/BankAccount.java
@@ -12,6 +12,7 @@ public class BankAccount {
         this("XNUMBER", 1.00, "XNAME", "XEMAIL", "XPHONE");
         System.out.println("Empty Constructor Called...");
     }
+
     public BankAccount(String number, double balance, String name, String email, String phone) {
         this.email = email;
         this.phone = phone;
@@ -44,39 +45,40 @@ public class BankAccount {
         return number;
     }
 
-//    public void setNumber(String number) {
-//        this.number = number;
-//    }
+    //    public void setNumber(String number) {
+    //        this.number = number;
+    //    }
 
     public double getBalance() {
         return balance;
     }
 
-//    public void setBalance(double balance) {
-//        this.balance = balance;
-//    }
-//
+    //    public void setBalance(double balance) {
+    //        this.balance = balance;
+    //    }
+    //
     public String getName() {
         return name;
     }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
+
+    //
+    //    public void setName(String name) {
+    //        this.name = name;
+    //    }
+    //
     public String getEmail() {
         return email;
     }
 
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
+    //    public void setEmail(String email) {
+    //        this.email = email;
+    //    }
+    //
     public String getPhone() {
         return phone;
     }
-//
-//    public void setPhone(String phone) {
-//        this.phone = phone;
-//    }
+    //
+    //    public void setPhone(String phone) {
+    //        this.phone = phone;
+    //    }
 }
--- src/Section7/src/ConstructorChallenge.java
+++ src/Section7/src/ConstructorChallenge.java
@@ -1,4 +1,5 @@
 public class ConstructorChallenge {
+
     private String name;
     private String email;
     private String phone;
