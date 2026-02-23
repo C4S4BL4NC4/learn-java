316fa4a2508544f6fb265c8840f08bbf2e26246d
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-02-23 11:02:03 +0300

    Add Student class with fields and methods; implement Main class to demonstrate usage

---
+++ src/Section7/src/RecordProject/src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            Student s = new Student(
+                "S93200" + i,
+                switch (i) {
+                    case 1 -> "Leon";
+                    case 2 -> "Rick";
+                    case 3 -> "Linda";
+                    case 4 -> "Susan";
+                    case 5 -> "Mary";
+                    default -> "Anonymous";
+                },
+                "05-11-1995",
+                "Java Masterclass"
+            );
+            System.out.println(s);
+        }
+    }
+}
--- src/Section7/src/RecordProject/src/Student.java
+++ src/Section7/src/RecordProject/src/Student.java
@@ -1,4 +1,69 @@
-package PACKAGE_NAME;
-
 public class Student {
+
+    // POJO == DTO in TS: Only has instance fields (getters and setters included).
+    private String id;
+    private String name;
+    private String dateOfBirth;
+    private String classList;
+
+    public Student(String id, String name, String dateOfBirth, String classList) {
+        this.id = id;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.classList = classList;
+    }
+
+    // Annotation
+    @Override
+    public String toString() {
+        return (
+            "Student{" +
+            "id='" +
+            id +
+            '\'' +
+            ", name='" +
+            name +
+            '\'' +
+            ", dateOfBirth='" +
+            dateOfBirth +
+            '\'' +
+            ", classList='" +
+            classList +
+            '\'' +
+            '}'
+        );
+    }
+
+    // Boilerplate code
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getClassList() {
+        return classList;
+    }
+
+    public void setClassList(String classList) {
+        this.classList = classList;
+    }
 }
