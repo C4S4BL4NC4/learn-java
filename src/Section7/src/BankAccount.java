b1c99d4e014e63c7673f24aa204564aa49460f27
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-02-23 07:26:20 +0300

    Implement BankAccount class with deposit and withdrawal methods; update Main class to demonstrate usage

--- src/Section7/src/BankAccount.java
+++ src/Section7/src/BankAccount.java
@@ -1,4 +1,62 @@
-package PACKAGE_NAME;
-
 public class BankAccount {
+
+    private String number;
+    private double balance; // Auto sets to 0.00
+    private String name;
+    private String email;
+    private String phone;
+
+    public void depositFunds(double depositAmount) {
+        this.balance += depositAmount;
+        System.out.println("Deposit of $" + depositAmount);
+    }
+
+    public void withdrawFunds(double withdrawAmount) {
+        if (this.balance - withdrawAmount < 0) {
+            System.out.println("Insufficient Funds!");
+        } else {
+            this.balance -= withdrawAmount;
+            System.out.println("Withdrawal of $" + withdrawAmount);
+        }
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
--- src/Section7/src/Main.java
+++ src/Section7/src/Main.java
@@ -3,5 +3,10 @@ public class Main {
     public static void main(String[] args) {
         Car car = new Car();
         car.describeCar();
+        
+        BankAccount AlicesAccount = new BankAccount();
+        AlicesAccount.withdrawFunds(100);
+        AlicesAccount.depositFunds(250);
+        AlicesAccount.withdrawFunds(50);
     }
 }
