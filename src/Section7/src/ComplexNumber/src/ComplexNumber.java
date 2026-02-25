488d17946818c0ce7d14cc84ba57d061c86dddef
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-02-25 15:15:15 +0300

    Implement ComplexNumber class with basic operations and add .gitignore file

--- src/Section7/src/ComplexNumber/src/ComplexNumber.java
+++ src/Section7/src/ComplexNumber/src/ComplexNumber.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
-
 public class ComplexNumber {
+
+    // write code here
+    private double real, imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public void setReal(double real) {
+        this.real = real;
+    }
+
+    public void setImaginary(double imaginary) {
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return this.real;
+    }
+
+    public double getImaginary() {
+        return this.imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.setReal(real + this.real);
+        this.setImaginary(imaginary + this.imaginary);
+    }
+
+    public void add(ComplexNumber ComplexNumber) {
+        this.setReal(this.real + ComplexNumber.getReal());
+        this.setImaginary(this.imaginary + ComplexNumber.getImaginary());
+    }
+
+    public void subtract(ComplexNumber ComplexNumber) {
+        this.setReal(this.real - ComplexNumber.getReal());
+        this.setImaginary(this.imaginary - ComplexNumber.getImaginary());
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.setReal(this.real - real);
+        this.setImaginary(this.imaginary - imaginary);
+    }
 }
