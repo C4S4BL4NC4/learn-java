0a896c0c0a030fa4052df755bb44ba146e8f90c2
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-03-30 18:53:09 +0300

    Add Shape and Rectangle classes with constructors; create .gitignore and vcs.xml files

--- src/Section7/src/theSuperKeyword/src/Rectangle.java
+++ src/Section7/src/theSuperKeyword/src/Rectangle.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+public class Rectangle extends Shape {
+    private int width;
+    private int height;
 
-public class Rectangle {
+    public Rectangle(int x, int y) {
+        this(x, y, 0, 0);
+    }
+
+    public Rectangle(int x, int y, int width, int height) {
+        super(x, y);
+        this.width = width;
+        this.height = height;
+    }
 }
--- src/Section7/src/theSuperKeyword/src/Shape.java
+++ src/Section7/src/theSuperKeyword/src/Shape.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Shape {
+    private int x;
+    private int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
