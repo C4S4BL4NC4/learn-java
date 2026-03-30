2c98bfdc78f3d0b00651d7db61bb7de66c8a66d3
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-03-30 19:46:46 +0300

    Add project configuration files and enhance Main class functionality

--- src/Section7/src/AllAboutStrings/src/Main.java
+++ src/Section7/src/AllAboutStrings/src/Main.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        printInformation("Hello World!");
+        printInformation("");
+    }
+
+    public static void printInformation(String string) {
+        int length = string.length();
+        if(string.isEmpty()) {
+            System.out.println("The string is empty!");
+            return;
+        }
+        System.out.printf("Length = %d %n", length);
+        System.out.printf("First char = %c %n", string.charAt(0));
+
+        System.out.printf("Last char = %c %n", string.charAt(length - 1));
+    }
 }
