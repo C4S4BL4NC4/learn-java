cdeb9942bac66b017c983de7c9b7f85795720a26
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-03-30 19:38:09 +0300

    Refactor Main class to include age calculation and formatted output

--- src/Section7/src/textFormating/src/Main.java
+++ src/Section7/src/textFormating/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        String 
+        int age = 35;
+        System.out.printf("Age: %.2f%n\nBirth Year: %d",(float)age, 2026-age);
+        String formatedString = String.format("Your are is %d", age);
+        System.out.println(formatedString);
     }
 }
