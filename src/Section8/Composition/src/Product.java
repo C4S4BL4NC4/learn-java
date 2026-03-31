5b9435fad5b6fb2f4f9820b502124cb886a1c8c3
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-03-31 03:17:11 +0300

    Add Product class with constructor; implement Main class structure; add project configuration files

--- src/Section8/Composition/src/Product.java
+++ src/Section8/Composition/src/Product.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Product {
+    private String model;
+    private String manufacturer;
+    private int width, height, depth;
+
+    public Product(String model, String manufacturer, int width, int height, int depth) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    
 }
