176de0bb4427527ef1d2bf0332588365f4ab171b
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-03-31 22:50:18 +0300

    Implement Bedroom, Bed, Ceiling, Lamp, and Wall classes with constructors and methods; update Main class to demonstrate usage

--- src/Section8/Challenges/Bedroom/src/Ceiling.java
+++ src/Section8/Challenges/Bedroom/src/Ceiling.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Ceiling {
+
+    private int height, paintedColor;
+
+    public Ceiling(int height, int painterColor) {
+        this.height = height;
+        this.paintedColor = painterColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPainterColor() {
+        return paintedColor;
+    }
 }
--- src/Section8/Challenges/Bedroom/src/Lamp.java
+++ src/Section8/Challenges/Bedroom/src/Lamp.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class Lamp {
+
+    private String style;
+    private boolean battery;
+    private int globRating;
+
+    public Lamp(String style, boolean battery, int globRating) {
+        this.style = style;
+        this.battery = battery;
+        this.globRating = globRating;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public boolean isBattery() {
+        return battery;
+    }
+
+    public int getGlobRating() {
+        return globRating;
+    }
+
+    public void turnOn() {
+        System.out.println("Turning the lights on");
+    }
 }
