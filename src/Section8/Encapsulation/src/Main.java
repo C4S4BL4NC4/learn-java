8f80bc0c835ce5939829dde4ea92695cb059c8d2
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-04-01 23:35:44 +0300

    Implement Printer class with methods for toner management and page printing

--- src/Section8/Encapsulation/src/Main.java
+++ src/Section8/Encapsulation/src/Main.java
@@ -9,5 +9,9 @@ public class Main {
 
         EnhancedPlayer alice = new EnhancedPlayer("Alice");
         System.out.println("Initial health is " + alice.healthRemaining());
+
+        Printer G3010 = new Printer(50, false);
+        G3010.printPages(50);
+        G3010.addToner(150);
     }
 }
--- src/Section8/Encapsulation/src/Printer.java
+++ src/Section8/Encapsulation/src/Printer.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
-
 public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted = 0;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        this.tonerLevel = tonerLevel;
+        this.duplex = duplex;
+    }
+
+    public int addToner(int tonerAmount) {
+        if ( tonerAmount < 0) {
+            this.tonerLevel = 0;
+        } else if (tonerAmount > 100) {
+            this.tonerLevel = 100;
+        } else {
+            this.tonerLevel = tonerAmount;
+        }
+        System.out.println("Current toner level: "+ this.tonerLevel);
+        return this.tonerLevel;
+    }
+
+    public int printPages(int pages) {
+        this.pagesPrinted = pages;
+        if (duplex) {
+            int sheets = pages/2;
+            System.out.println("Duplex: required sheets: "+ sheets);
+            return sheets;
+        } else {
+            System.out.println("is Not Duplex: required amount sheets: "+ pages);
+            return pages;
+        }
+    }
 }
