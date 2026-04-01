88d3df21f763627871b5d43ab4906e629c7c8622
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-04-01 22:11:59 +0300

    Implement EnhancedPlayer class with health management and update Main class to demonstrate usage

--- src/Section8/Encapsulation/src/Main.java
+++ src/Section8/Encapsulation/src/Main.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static void main(String[] args) {
+        Player player = new Player("Tim", 20, "Sword");
+
+        int damage = 10;
+        player.loseHealth(damage);
+        System.out.println("Remaining health = " + player.healthRemaining());
+
+        EnhancedPlayer alice = new EnhancedPlayer("Alice");
+        System.out.println("Initial health is " + alice.healthRemaining());
+    }
 }
--- src/Section8/Encapsulation/src/Player.java
+++ src/Section8/Encapsulation/src/Player.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
+// No encapsulation - AKA public fields. Bypassing checks, and set conditons.
 public class Player {
+
+    public String name;
+    public int health;
+    public String weapon;
+
+    public Player(String name, int health, String weapon) {
+        this.name = name;
+        this.health = health;
+        this.weapon = weapon;
+    }
+
+    public void loseHealth(int damage) {
+        health = health - damage;
+        if (health <= 0) {
+            System.out.println("Player knocked out of the game");
+        }
+    }
+
+    public int healthRemaining() {
+        return health;
+    }
+
+    public void restoreHealth(int extraHealth) {
+        health = health + extraHealth;
+        if (health > 100) {
+            System.out.println("Player health restored.");
+            health = 100;
+        }
+    }
 }
