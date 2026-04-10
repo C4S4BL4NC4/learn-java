1cb4233d0b67eba4249917cd72869562c475ad68
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-04-10 17:39:02 +0300

    Implement array sorting and searching logic in Main class; add project configuration files and .gitignore for IDEs

--- src/Section9/SeachingArrays/src/Main.java
+++ src/Section9/SeachingArrays/src/Main.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Main {
+
+    public static void main(String[] args) {
+        String[] sArray = { "Alice", "Mark", "Dominique", "Rosie", "Hannah", "Jake", "Bobby" };
+        Arrays.sort(sArray);
+        System.out.println(Arrays.toString(sArray));
+
+        if (Arrays.binarySearch(sArray, "Ruby") >= 0) {
+            System.out.println("Found mark");
+        } else {
+            System.out.println("Not found!");
+        }
+
+        int[] n1 = { 1, 2, 3, 4, 5 };
+        int[] n2 = { 1, 2, 3, 4, 5 };
+
+        // Arrays.equal checks for length and positions
+        if(Arrays.equals(n1, n2)) {
+            System.out.println("Arrays are equal!");
+        } else {
+            System.out.println("Arrays not equal!");
+        }
+    }
 }
