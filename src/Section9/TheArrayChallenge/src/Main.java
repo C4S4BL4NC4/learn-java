e7a2564894df3cd303322a1d4d0d0d4184425bcf
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-04-10 18:21:36 +0300

    Implement main logic for random array generation and sorting; add VCS configuration file

--- src/Section9/TheArrayChallenge/src/Main.java
+++ src/Section9/TheArrayChallenge/src/Main.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
+
+    public static void main(String[] args) {
+        int[] firstArray = getRandomArray(10);
+        System.out.println(Arrays.toString(firstArray));
+        Arrays.sort(firstArray);
+        System.out.println(Arrays.toString(firstArray));
+
+        int[] mArray = Arrays.copyOf(firstArray, 10);
+        System.out.println(Arrays.toString(mArray));
+    }
+
+    private static int[] getRandomArray(int len) {
+        Random random = new Random();
+        int[] intArray = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            intArray[i] = random.nextInt(100);
+        }
+        return intArray;
+    }
 }
