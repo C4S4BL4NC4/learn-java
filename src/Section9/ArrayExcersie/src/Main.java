75043978ef2522546b4648201eaaa0384176e517
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-04-14 10:49:14 +0300

    Add initial project structure and configuration files for ArrayChallenges and ArrayExcersie modules Implement main methods and utility functions in Main classes for ArrayChallenges and ArrayExcersie Add .gitignore files for ArrayChallenges and ArrayExcersie modules Add IntelliJ IDEA and Eclipse configuration files for ArrayChallenges module

--- src/Section9/ArrayExcersie/src/Main.java
+++ src/Section9/ArrayExcersie/src/Main.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static void main(String[] args) {}
+
+    private static int[] readIntegers(int[] intList) {
+        return intList;
+    }
+
+    private static int findMin(int... intList) {
+        int min = intList[0];
+
+        for (int i : intList) {
+            if (min < intList[i]) {
+                min = intList[i];
+            } else {
+                continue;
+            }
+        }
+        return min;
+    }
 }
