4b40bd90577f2b276223533aea30d188a567d6ce
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-04-14 17:12:48 +0300

    Add Main class implementation for 2D array display and create .gitignore and vcs.xml files

--- src/Section9/2DArrays/src/Main.java
+++ src/Section9/2DArrays/src/Main.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Main {
+    public static void main(String[] args) {
+        int[][] array = new int[3][3];
+        System.out.println(Arrays.toString(array));
+
+        for(int[] outer : array) {
+            System.out.println(Arrays.toString(outer));
+        }
+
+        System.out.println(Arrays.deepToString(array));
+
+    }
 }
