c91d0eba23d2c4b01ba4b3471e10882dec9d6009
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-04-14 11:09:32 +0300

    Refactor Main class to implement array input and minimum value calculation

--- src/Section9/ArrayExcersie/src/Main.java
+++ src/Section9/ArrayExcersie/src/Main.java
@@ -1,17 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        System.out.println("ARRAY METHODS MINIMUM CHALLENGE");
+        int[] returnedArray = readIntegers();
+        System.out.println(Arrays.toString(returnedArray));
 
-    private static int[] readIntegers(int[] intList) {
-        return intList;
+        int returnedMin = findMin(returnedArray);
+        System.out.println("Min = " + returnedMin);
+    }
+
+    private static int[] readIntegers() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a list of integers, seperated by commas: ");
+        String input = scanner.nextLine();
+        String[] splits = input.split(",");
+        int[] vals = new int[splits.length];
+
+        for (int i = 0; i < splits.length; i++) {
+            vals[i] = Integer.parseInt(splits[i].trim());
+        }
+        return vals;
     }
 
     private static int findMin(int... intList) {
         int min = intList[0];
 
         for (int i : intList) {
-            if (min < intList[i]) {
-                min = intList[i];
+            if (min > i) {
+                min = i;
             } else {
                 continue;
             }
