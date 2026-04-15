349f5fa23e363310d72051c6752600840a2123b3
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-04-15 23:46:23 +0300

    Format code for consistency in Main class

--- src/Section9/2DArrays/src/Main.java
+++ src/Section9/2DArrays/src/Main.java
@@ -1,15 +1,15 @@
 import java.util.Arrays;
 
 public class Main {
+
     public static void main(String[] args) {
         int[][] array = new int[3][3];
         System.out.println(Arrays.toString(array));
 
-        for(int[] outer : array) {
+        for (int[] outer : array) {
             System.out.println(Arrays.toString(outer));
         }
 
         System.out.println(Arrays.deepToString(array));
-
     }
 }
