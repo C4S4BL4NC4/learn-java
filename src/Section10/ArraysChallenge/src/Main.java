de38f962c1e0c9825b6e7d96ad4d89389f84de3b
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-06-13 06:54:02 +0300

    Refactor Main class to enhance user interaction and streamline item management

--- src/Section10/ArraysChallenge/src/Main.java
+++ src/Section10/ArraysChallenge/src/Main.java
@@ -1,40 +1,36 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Scanner word = new Scanner(System.in);
         ArrayList<String> items = new ArrayList<>();
+        boolean flag = true;
 
-        while (true) {
+        while (flag) {
             System.out.println("items -> " + items.toString());
             System.out.println(
-                "0 Shutdown" + "\n1 add items" + "\n2 remove items" + "\nEnter a number of action: "
+                "0- Shutdown" +
+                    "\n1- Add items" +
+                    "\n2- Remove items" +
+                    "\nEnter a number of action: "
             );
             int option = scanner.nextInt();
-            if (option > 2 || option < 0) {
-                System.out.println("Invalid option. Try again");
-                continue;
-            }
-            if (option == 0) {
-                System.out.println("Quitting...");
-                break;
-            }
-            if (option == 1) {
-                System.out.println("Enter a new Item: ");
-                String newItem = scanner.nextLine();
-                items.add(newItem);
-            }
-            if (option == 2) {
-                if (items.isEmpty()) {
-                    System.out.println("Items array is empty");
+            switch (Integer.parseInt(scanner.nextLine())){
+                default -> flag = false;
+                case 1 -> {
+                    System.out.println("Enter Items separated by commas: ");
+                    String[] basket = scanner.nextLine().split(",");
                 }
-                System.out.println("Enter item name to remove: ");
-                String removableItem = scanner.nextLine();
-                items.remove(items.indexOf(removableItem));
-                continue;
+                case 2 -> {
+                    break;
+                }
             }
+            items.sort(Comparator.naturalOrder());
+            System.out.println(items);
         }
     }
 }
