4be3a7d4e6d2797ded9b146988942c069716af9e
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-06-16 01:26:54 +0300

    Add LinkedLists module configuration files and implement LinkedList example in Main class

--- src/Section10/LinkedLis/src/Main.java
+++ src/Section10/LinkedLis/src/Main.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+import java.util.LinkedList;
 
 public class Main {
+    public static void main(String[] args) {
+        // LinkedList<String> placesToVisit = new LinkedList<>();
+        var placesToVisit = new LinkedList<String>();
+        placesToVisit.add("Istanbul");
+        placesToVisit.add(0, "Bodrum");
+
+        // Add Elements
+        placesToVisit.addFirst("Kayseri");
+        placesToVisit.addLast("Hatay");
+        System.out.println(placesToVisit);
+
+        // Add Queue methods
+        placesToVisit.offer("Izmir");
+        placesToVisit.offerFirst("Kocali");
+        placesToVisit.offerLast("Bursa");
+        System.out.println(placesToVisit);
+
+        // Add Stack Methods
+        placesToVisit.push("Antalya");
+        System.out.println(placesToVisit);
+
+        // Remove
+        placesToVisit.remove(1);
+        placesToVisit.remove("Hatay");
+        placesToVisit.remove();
+        placesToVisit.removeFirst();
+        placesToVisit.removeLast();
+        System.out.println(placesToVisit);
+
+        // Remove stack
+        placesToVisit.pop();
+        System.out.println(placesToVisit);
+    }
 }
