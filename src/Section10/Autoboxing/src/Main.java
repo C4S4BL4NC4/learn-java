48dbe17d8f9800ab41c5f65c165ad8028bfeaaa9
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-06-18 10:24:16 +0300

    Add boxing/unboxing examples to Main
    Introduce examples demonstrating manual boxing (Integer.valueOf and new Integer), explicit unboxing (intValue), autoboxing (assigning int to Integer) and auto-unboxing in Main.main to illustrate Java boxing/unboxing behavior.

--- src/Section10/Autoboxing/src/Main.java
+++ src/Section10/Autoboxing/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     static void main(String[] args) {
 
+        // Manual boxing
+        Integer boxed = Integer.valueOf(23);
+        Integer depBox = new Integer(3);
+
+        // Unboxing
+        int unbox = boxed.intValue();
+
+        // Autoboxing
+        Integer auto = 6;
+        int autounbox = auto;
     }
 }
