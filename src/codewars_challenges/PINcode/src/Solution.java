46c52ff0e30522ae01534ad115a96c779f7e79cc
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-06-26 16:40:23 +0300

    Implement PIN validation logic and add project configuration files

--- src/codewars_challenges/PINcode/src/Solution.java
+++ src/codewars_challenges/PINcode/src/Solution.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Solution {
+
+    // ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+    static void main(String[] args) {
+        System.out.println(validatePin("1234"));
+        System.out.println(validatePin("a234"));
+        System.out.println(validatePin("1234567"));
+        System.out.println(validatePin("-1234"));
+        System.out.println(validatePin("1.456"));
+        System.out.println(validatePin("aaaaaa"));
+        System.out.println(validatePin("123"));
+    }
+
+    public static boolean validatePin(String pin) {
+        // Your code here...
+        return pin.matches("^\\d{4}(\\d{2})?$");
+    }
 }
