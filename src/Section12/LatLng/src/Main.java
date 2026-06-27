cf78934e4a65c61d1f441aa3230eaa318f63a007
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-06-28 01:09:43 +0300

    Refactor Main class: streamline park and river rendering by utilizing Layer class

--- src/Section12/LatLng/src/Main.java
+++ src/Section12/LatLng/src/Main.java
@@ -6,8 +6,19 @@ public class Main {
             new Park("Yellowstone", "33.331, 55.5512"),
             new Park("Duhrosie", "324.67, 420.69"),
         };
-        for (var i : usNationalParks) {
-            System.out.println(i.toString());
-        }
+
+        Layer<Park> parkLayer = new Layer<>(usNationalParks);
+        parkLayer.renderLayer();
+
+        var usMajorRivers = new River[] {
+            new River("Mississippi", "47.4, 56.53", "12.40, 10.12", "-95.13, -69.204"),
+            new River("Missouri", "44.4, 66.53", "22.40, 17.12", "-9.13, -6.204"),
+        };
+
+        Layer<River> riverLayer = new Layer<>(usMajorRivers);
+        riverLayer.addElements(
+            new River("Delaware", "44.44, 55.55", "22.22, 77.77", "52.52, 76.76")
+        );
+        riverLayer.renderLayer();
     }
 }
