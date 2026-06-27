e2edd2cf52620a843e1390873ced99d9e022d658
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-06-28 00:33:25 +0300

    Fix Main class: correct syntax for Park array initialization and enhance output loop

--- src/Section12/LatLng/src/Main.java
+++ src/Section12/LatLng/src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
 
     static void main(String[] args) {
-
         System.out.println("Latitude Longitude Challenge:");
-        var usNationalParks = new Park[]{
-                new Park("Yellowstone", "33.331, 55.5512");
-                new Park("Duh Park", "324.67, 420.69");
+        var usNationalParks = new Park[] {
+            new Park("Yellowstone", "33.331, 55.5512"),
+            new Park("Duhrosie", "324.67, 420.69"),
+        };
+        for (var i : usNationalParks) {
+            System.out.println(i.toString());
         }
     }
 }
