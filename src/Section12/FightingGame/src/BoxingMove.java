1bb72a797c0a5838555446b6771e73b46741c8b5
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-07-03 23:06:13 +0300

    Add UNBLOCKABLE move to BoxingMove enum with corresponding damage value

--- src/Section12/FightingGame/src/BoxingMove.java
+++ src/Section12/FightingGame/src/BoxingMove.java
@@ -1,13 +1,15 @@
 public enum BoxingMove implements Move {
     JAB,
     CROSS,
-    HOOK;
+    HOOK,
+    UNBLOCKABLE;
     
     public int damage() {
         return switch (this) {
             case JAB -> 5;
             case CROSS -> 8;
             case HOOK -> 10;
+            case UNBLOCKABLE -> 55;
         };
     }
 }
