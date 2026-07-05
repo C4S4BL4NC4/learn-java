a7a4032212875ca58a6ea0db956b429c694e5f45
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-07-06 00:39:55 +0300

    Add an additional Hello World print statement in main method

--- src/helloworld/src/Main.java
+++ src/helloworld/src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.printf("Hello World!");
         System.out.printf("Hello World!");
         System.out.printf("Hello World!");
+        System.out.printf("Hello World!");
     }
 }
