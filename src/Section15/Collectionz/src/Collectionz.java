c6e5168183ae6da3cc013bb1f0a3f4ddce9a78aa
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-08-05 15:27:37 +0300

    Implement Collectionz class with list operations and add Palantir Java Format configuration

--- src/Section15/Collectionz/src/Collectionz.java
+++ src/Section15/Collectionz/src/Collectionz.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Collectionz {
+
     public static void main(String[] args) {
+
         System.out.println("Collectionz.main");
+        List<String> list = new ArrayList<>();
+        String[] array = {"Roger", "Carol", "Benzi", "Alex"};
+
+        list.addAll(Arrays.asList(array));
+        System.out.println("list = " + list);
+        list.add("Garry");
+        System.out.println("The list contains Garry? " + list.contains("Garry"));
+
+        list.removeIf(s -> s.charAt(0) == 'G');
+        System.out.println("list = " + list);
+        System.out.println("The list contains Garry? " + list.contains("Garry"));
     }
 }
