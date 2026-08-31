8d94e8f60722fe2e0a6f1b8a8687cca806fe327b
chromaticabberation <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-08-31 18:24:32 +0300

    -------------------------------------

--- src/Section16/GameFramework/src/dev/lpa/Main.java
+++ src/Section16/GameFramework/src/dev/lpa/Main.java
@@ -1,8 +1,15 @@
 package dev.lpa;
 
+import dev.lpa.game.GameConsole;
+import dev.lpa.game.ShooterGame;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Main.main");
+
+        var console = new GameConsole<>(new ShooterGame("Shooter: The Game"));
+        int playerIndex = console.addPlayer();
+        console.playGame(playerIndex);
     }
 }
--- src/Section16/GameFramework/src/dev/lpa/game/GameConsole.java
+++ src/Section16/GameFramework/src/dev/lpa/game/GameConsole.java
@@ -1,4 +1,46 @@
 package dev.lpa.game;
 
-public class GameConsole {
+import java.util.Scanner;
+
+public class GameConsole<T extends Game<? extends Player>> {
+
+    private final T game;
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public GameConsole(T game) {
+        this.game = game;
+    }
+
+    public int addPlayer() {
+        System.out.print("Enter your playing game: ");
+        var name = scanner.nextLine();
+
+        System.out.printf("Welcome to %s, %s!%n".formatted(game.getGameName(), name));
+        return game.addPlayer(name);
+    }
+
+    public void playGame(int playerIndex) {
+        boolean done = false;
+        while (!done) {
+            var gameActions = game.getGameActions(playerIndex);
+            System.out.println("Select one of the following actions: ");
+            for (var c : gameActions.keySet()) {
+                var prompt = gameActions.get(c).prompt();
+                System.out.println("\t" + prompt + " (" + c + ")");
+            }
+
+            System.out.println("Enter Next Action: ");
+
+            var nextMove = scanner.nextLine().charAt(0);
+            var gameAction = gameActions.get(nextMove);
+
+            if (gameAction != null) {
+                System.out.println("-------------------------------------");
+                done = game.executeGameAction(playerIndex, gameAction);
+                if (!done) {
+                    System.out.println("-------------------------------------");
+                }
+            }
+        }
+    }
 }
