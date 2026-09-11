35e85901e69901ad688490595d0a5b073350f6b7
chromaticabberation <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-09-11 21:29:35 +0300

    streams edit
    streams edit

--- src/Section17/StreamsInter/src/dev/lpa/Main.java
+++ src/Section17/StreamsInter/src/dev/lpa/Main.java
@@ -1,14 +1,27 @@
 package dev.lpa;
 
+import java.util.Random;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Main.main");
         IntStream.iterate((int) 'A', i -> i <= (int) 'z', i -> i + 1)
-//                .skip(5)
-                .dropWhile(i -> i <= 'E')
                 .filter(Character::isAlphabetic)
+                .map(Character::toUpperCase)
+                .distinct()
+//                .skip(5)
+//                .dropWhile(i -> i <= 'E') // Stops when predicate becomes false
+//                .takeWhile(i -> i < 'a')
                 .forEach(s -> System.out.printf("%c%n", s));
+
+        Random random = new Random();
+
+        Stream.generate(() -> random.nextInt((int) 'A', (int) 'Z' + 1))
+                .limit(50)
+                .distinct()
+                .sorted()
+                .forEach(c -> System.out.printf("%c%n", c));
     }
 }
